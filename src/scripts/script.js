59c5bbcc5800fcb28fcd7a48ef281ed4fc9e52c2
Ruan Carlos <88854028+rucp@users.noreply.github.com>
2022-03-08 16:00:30 -0300

    update

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,36 @@
-function submit() {
-    let firstName = document.forms["form"]["firstName"].value;
-    let lastName = document.forms["form"]["lastName"].value;
-    let email = document.forms["form"]["email"].value;
-    let password = document.forms["form"]["password"].value;
+function validar() {
+    let firstName = document.getElementById('firstname');
+    let lastName = document.getElementById('lastname');
+    let email = document.getElementById('email');
+    let pass = document.getElementById('password');
+    let input = document.getElementsByTagName('input');
 
-    submit = true;
 
-    if (firstName == null || firstName == '') {
-        nameError = "Please enter your name";
-        document.getElementById("txtname").innerHTML = nameError;
-        submit = false;
+    if(firstName.value == '') {
+        txtname.innerHTML = 'First Name cannot be empty';
+        firstName.style.border = "2px solid #F0787A";
+        firstName.style.background = "url('src/assets/images/icon-error.svg') no-repeat content-box right";
+        
+        // document.querySelector("input").style.border = "red";
     }
-    return submit;
+
+    if(lastName.value == '') {
+        txtlastname.innerHTML = 'Last Name cannot be empty';
+        lastName.style.border = "2px solid #F0787A";
+        lastName.style.background = "url('src/assets/images/icon-error.svg') no-repeat content-box right";
+    }
+
+    if(email.value == '') {
+        txtemail.innerHTML = 'Looks like this is not an email';
+        email.style.border = "2px solid #F0787A"
+        email.style.background = "url('src/assets/images/icon-error.svg') no-repeat content-box right";
+        email.placeholder = "email@example/com";
+
+    }
+
+    if(pass.value == '') {
+        txtpass.innerHTML = 'Password cannot be empty'
+        pass.style.border = "2px solid #F0787A"
+        pass.style.background = "url('src/assets/images/icon-error.svg') no-repeat content-box right";
+    }
 }
