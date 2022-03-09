aa51431c09c24df7bdf82acbf76bb80fe8bef416
Ruan Carlos <88854028+rucp@users.noreply.github.com>
2022-03-09 12:38:23 -0300

    update

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,21 +5,33 @@ function validar() {
     let pass = document.getElementById('password');
     let input = document.getElementsByTagName('input');
 
-
+    // first name
     if(firstName.value == '') {
         txtname.innerHTML = 'First Name cannot be empty';
         firstName.style.border = "2px solid #F0787A";
         firstName.style.background = "url('src/assets/images/icon-error.svg') no-repeat content-box right";
-        
-        // document.querySelector("input").style.border = "red";
     }
 
+    if(firstName.value != '') {
+        firstName.style.border = "1px solid #b9b6d3";
+        txtname.innerHTML = '';
+        firstName.style.background = "none";
+    }
+
+    // last name
     if(lastName.value == '') {
         txtlastname.innerHTML = 'Last Name cannot be empty';
         lastName.style.border = "2px solid #F0787A";
         lastName.style.background = "url('src/assets/images/icon-error.svg') no-repeat content-box right";
     }
 
+    if(lastName.value != '') {
+        lastName.style.border = "1px solid #b9b6d3";
+        txtlastname.innerHTML = '';
+        lastName.style.background = "none";
+    }
+
+    // email 
     if(email.value == '') {
         txtemail.innerHTML = 'Looks like this is not an email';
         email.style.border = "2px solid #F0787A"
@@ -28,9 +40,22 @@ function validar() {
 
     }
 
+    if(email.value != '') {
+        email.style.border = "1px solid #b9b6d3";
+        txtemail.innerHTML = '';
+        email.style.background = "none";
+    }
+
+    // password
     if(pass.value == '') {
         txtpass.innerHTML = 'Password cannot be empty'
         pass.style.border = "2px solid #F0787A"
         pass.style.background = "url('src/assets/images/icon-error.svg') no-repeat content-box right";
+    }
+
+    if(pass.value != '') {
+        pass.style.border = "1px solid #b9b6d3";
+        txtpass.innerHTML = '';
+        pass.style.background = "none";
     }
 }
